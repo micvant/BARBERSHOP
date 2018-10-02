e40ee4e34b4ed50214a3e1f1a3e8f381561a47a8
Micvant <Micvant@mail.ru>
2018-10-03 08:29:19 +0900

    Test js

--- js/test.js
+++ js/test.js
@@ -1,13 +1,13 @@
-	var res;
-	var number;
-	var floatNumber;
+	
+	/*document.write("Hello, world!");*/
 	document.write("Hello, world! </br>");
 	document.write("Variable number = " + string (res));
-	/*document.write("Hello, world!");*/
 	floatNumber = 2.2;
 	number = 1;
 	res ++;
 	res += number;
 	res --;
 	res = number + floatNumber;
-	
+	var floatNumber;
+	var number;
+	var res;
